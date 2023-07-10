871c976141a27020bcae5a97da7cd07603828a19
ariel.duarte <arielpy@gmail.com>
2023-07-09 21:15:35 -0300

    Added project base next.js 13 and primeReact UI

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Sakai Template</h1>
+      <h3>Powered by PrimeReact and Next.js 13</h3>
+      <h6>Created by Reynaldo Ariel Duarte (c)2023</h6>
+    </>
+  );
+}
